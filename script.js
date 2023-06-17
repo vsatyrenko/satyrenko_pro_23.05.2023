da73e7a7ab1bf3d9cd69470d1163d189775b2a87
vsatyrenko <wsatyrenko@gmail.com>
2023-06-17 13:42:27 -0500

    fix hw-03-task-2

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-//HW-03-task3
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-switch (numOrStr) {
-    case null:
-        console.log('ви відмінили');
-        break;
-    case '': 
-        console.log('Empty String');
-        break;
-    case !isNaN(numOrStr) || numOrStr:
-        console.log(' number is Ba_NaN');
-        break;
- default:
-    console.log('OK!')
-}
+// HW-03 Task 02
+const birth = prompt('Введіть ваш рік народження');
+const age = 2023 - birth;
+const city = prompt('В якому місті ви живите');
+const sports = prompt('Введіть ваш улюбленний вид спорту')
 
-// let numOrStr = prompt('input number or string');
-// console.log(numOrStr)
+let howOld;
+let userCity;
+let typeOfSport;
+if (birth) {
+    howOld = `Ваш вік ${age}`}
+    else
+    howOld = 'Шкода, що Ви не захотіли ввести свій рік народження';
 
-// if(numOrStr === null) {
-//     console.log('ви відмінили')
-// } else if( numOrStr.trim() === '' ) {
-//     console.log('Empty String');
-// } else if( isNaN( +numOrStr ) ) {
-//     console.log(' number is Ba_NaN')
-// } else {
-//     console.log('OK!')
-// }
+if (city === 'Київ') {
+        userCity = `Ти живеш у столиці ${city}`}
+    else if (city === 'Вашингтон') {
+        userCity = `Ти живеш у столиці ${city}`}
+    else if (city === 'Лондон') {
+        userCity = `Ти живеш у столиці ${city}`}
+    else if (city) {
+        userCity = `Ти живеш у місті ${city}`
+    }else 
+        userCity = 'Шкода, що Ви не захотіли ввести своє місто';
+        
+if(sports === 'Футбол') {
+        typeOfSport = 'Круто! Хочеш стати Андрієм Шевченко?'
+        }
+    else if(sports === 'Бокс') {
+        typeOfSport = 'Круто! Хочеш стати Віталієм чи Володимиром Кличко?'
+        }
+    else if(sports === 'Важка атлетика') {
+        typeOfSport = 'Круто! Хочеш стати Василем Вірастюком?'
+        }
+    else
+        typeOfSport = 'Шкода, що Ви не захотіли ввести свій улюбленний вид спорту';
+
+
+alert (`${howOld} 
+${userCity}
+${typeOfSport}`);
