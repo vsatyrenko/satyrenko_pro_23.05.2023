e34839ed020c90120f007f46661b567aef7fc41f
vsatyrenko <wsatyrenko@gmail.com>
2023-06-20 19:51:50 -0500

    fix

--- script.js
+++ script.js
@@ -14,16 +14,3 @@ switch (true) {
  default:
     console.log('OK!')
 }
-
-// let numOrStr = prompt('input number or string');
-// console.log(numOrStr)
-
-// if(numOrStr === null) {
-//     console.log('ви відмінили')
-// } else if( numOrStr.trim() === '' ) {
-//     console.log('Empty String');
-// } else if( isNaN( +numOrStr ) ) {
-//     console.log(' number is Ba_NaN')
-// } else {
-//     console.log('OK!')
-// }
