6035ed561ecb10d82f8d088219a3ecf555cd438c
vsatyrenko <wsatyrenko@gmail.com>
2023-06-20 19:50:59 -0500

    fix

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-// task-01
-// const name = prompt('Як вас звати?');
-// alert (`Hello, ${name}! How are you?`);
+//HW-03-task3
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+switch (true) {
+    case numOrStr === null:
+        console.log('ви відмінили');
+        break;
+    case ( numOrStr.trim() === '' ): 
+        console.log('Empty String');
+        break;
+    case ( isNaN( +numOrStr ) ):
+        console.log(' number is Ba_NaN');
+        break;
+ default:
+    console.log('OK!')
+}
 
-// task-02
-// const firstNumber = Number(prompt('Введіть перше число'));
-// const secondNumber = Number(prompt('Введіть друге число'));
-// alert (`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}
-// ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}
-// ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}
-// ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`)
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
 
-// task-03
-// const value = prompt('Введіть якесь значення');
-// const _value = prompt('Введіть якесь значення');
-// if (value === _value) {
-//     console.log(true)
-// } else ( console.log(false))
-
-// task-04
-// const firstNumber = Number(prompt('Введіть перше число'));
-// const secondNumber = Number(prompt('Введіть друге число'));
-// const thirddNumber = Number(prompt('Введіть друге число'));
-// const midleNumber = firstNumber + secondNumber + thirddNumber / 3;
-// alert(`Середнє арефметичне цих чисел ${midleNumber}`)
-
-//task-05
-const a = Number(prompt('Введіть пятизначне число'));
-const fivethNumber = a % 10;
-const fourthNumber = ((a - fivethNumber)/10)%10;
-const thirdNumber = ((a - (fourthNumber *10 + fivethNumber))/100)%10;
-const secondNumber = ((a - (thirdNumber *100 + fourthNumber *10 + fivethNumber))/1000)%10;
-const firstNumber = ((a - (secondNumber*1000 + thirdNumber *100 + fourthNumber *10 + fivethNumber))/10000)%10; 
-alert (`${fivethNumber} ${fourthNumber} ${thirdNumber} ${secondNumber} ${firstNumber}`);
+// if(numOrStr === null) {
+//     console.log('ви відмінили')
+// } else if( numOrStr.trim() === '' ) {
+//     console.log('Empty String');
+// } else if( isNaN( +numOrStr ) ) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
