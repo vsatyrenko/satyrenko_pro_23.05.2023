eb293985c9d92b8e74d9f0a220a93d003bb2e321
vsatyrenko <wsatyrenko@gmail.com>
2023-06-27 21:25:00 -0500

    hw-04

--- script.js
+++ script.js
@@ -1,16 +1,107 @@
-//HW-03-task3
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-switch (true) {
-    case numOrStr === null:
-        console.log('ви відмінили');
-        break;
-    case ( numOrStr.trim() === '' ): 
-        console.log('Empty String');
-        break;
-    case ( isNaN( +numOrStr ) ):
-        console.log(' number is Ba_NaN');
-        break;
- default:
-    console.log('OK!')
-}
+//Task-01
+// const a = 10;
+// const b = 5;
+
+// if (a > b) {
+//     console.log(`Число ${a} більше за число ${b}`)
+// };
+
+//Task-02
+// const kilometr = 400;
+// const pound = 2 * 0.305 * 1000;
+
+// if (kilometr < pound) {
+//     console.log(` Відстань ${kilometr} кілометрів меньша за відстань ${pound} фунтів`)
+// } else {
+//     console.log(`Відстань ${pound} фунтів більша за відстань ${kilometr} кілометрів`)
+// };
+
+//Task-03
+// const a = 30;
+// const b = 5;
+// if (a % b === 0)  {
+//     console.log('так ділиться')
+// } else {
+//     console.log('не ділиться')
+// };
+
+// const _b = 5;
+// const _a = 30;
+// if (_b % _a === 0)  {
+//     console.log('так ділиться')
+// } else {
+//     console.log('не ділиться')
+// };
+
+//Task-04
+// const number = 1234;
+// lastNumber = number % 10;
+// if(lastNumber % 2 === 0) {
+// console.log(`Це число закінчується парною цифрою ${lastNumber}`)
+// }else {
+// console.log(`Це число не закінчується парною цифрою це число ${lastNumber}`)
+// };
+
+//Task-05
+// const c = 45;
+// a = c / 10;
+// b= c % 10;
+
+// if (a > b) {
+//     console.log('перше число більше')
+
+//  } else {
+//     console.log('друге число більше')
+//  };
+
+//Task-06
+// const num = 123;
+//  if(num % 2 === 0) {
+//     console.log('сума парная')
+//  } else {
+//     console.log('сумма не парная')
+// };
+
+// const number = 123;
+// const a = number / 100;
+// const b = number % 100 / 10;
+// const c = number % 10;
+// const value = a + b + c;
+// if (value % 5 === 0) {
+//     console.log('кратне 5')
+// } else {
+//     console.log('не кратне 5')
+// }
+
+// const c = 123;
+// const a = c / 100;
+// const b = c % 100 / 10;
+// const d = c % 10;
+// if(a * b * d > 100 ) {
+//     console.log('не більше 100')
+// } else {
+//     console.log('більше 100')
+// };
+
+//Task-07 
+// const num = 654;
+// const num1 = num / 100;
+// const num2 = num % 100 / 10;
+// const num3 = num % 10;
+
+// if (num1 === num2 === num3) {
+//     console.log('так правда є однакові')
+// } else {
+//     console.log('ні не правда не має однакових')
+// };
+
+//Task-08
+const num = 123321;
+const num1 = 147741;
+
+if (num === num1) {
+    console.log('так вони дзеркальні')
+} else {
+    console.log('вони не дзеркальні')
+};
+    
