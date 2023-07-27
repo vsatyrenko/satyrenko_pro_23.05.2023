d99e53739beeb8bd2b5f958f1f1012cca05f83d9
vsatyrenko <wsatyrenko@gmail.com>
2023-07-26 20:12:40 -0500

    fix

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const input = prompt("Введіть мінімум 6 елементів");
-const array = input.split(" ");
+const array =  [ ]; 
+const size = prompt('Введіть довжину масива');
+for ( let i = 0; i < size; i ++)  {
+	array [ i ]  =  prompt ( ' Введіть елементи '  +  ( i + 1 )); 
+}
+console.log (array);
+
 const sort = array.sort();
+console.log(sort);
+
 const delet = array.splice(2,5);
 
-console.log(array);
+console.log(delet);
+
+console.log(` vbfdhjbhj${array}`);
+
+
+
+
